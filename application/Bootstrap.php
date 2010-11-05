1ebb6b82c598d6452ade07a70a785e7ee3c7d7aa
marcinwol <marcin@y7mail.com>
2010-11-05 21:24:27 +0800

    Just added layout

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -3,6 +3,11 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 
+        protected function _initDoctype() {
+            $this->bootstrap('view');
+            $view = $this->getResource('view');
+            $view->doctype('XHTML1_STRICT');
+        }
 
 }
 
