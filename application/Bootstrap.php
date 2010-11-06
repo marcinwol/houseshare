df222173a30d941299916b048ffe25d0355d6be1
marcinwol <marcin@y7mail.com>
2010-11-06 15:55:14 +0800

    Added possibility of using en and pl languages. For now, only in application.ini it can be specified which language to use. There need to be some links or way of changing the languages added.

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,13 +1,49 @@
 <?php
 
-class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
-{
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
 
-        protected function _initDoctype() {
-            $this->bootstrap('view');
-            $view = $this->getResource('view');
-            $view->doctype('XHTML1_STRICT');
-        }
+    protected function _initDoctype() {
+        $this->bootstrap('view');
+        $view = $this->getResource('view');
+        $view->doctype('XHTML1_STRICT');
+    }
+
+    protected function _initRoutes() {
+        $locale = $this->getResource('locale');
+        // Create route with language id (lang)
+        $routeLang = new Zend_Controller_Router_Route(
+                        ':lang',
+                        array(
+                            'lang' => $locale->getLanguage()
+                        ),
+                        array('lang' => '[a-z]{2}')
+        );
+    }
+
+    protected function _initTranslate() {
+        // We use the Polish locale as an example
+        $this->bootstrap('locale');
+        $locale = $this->getResource('locale');
+
+        Zend_Registry::set('Zend_Locale', $locale);
+        $langLocale = $locale;
+        // Create Session block and save the locale
+        //  $session = new Zend_Session_Namespace('session');
+        //  $langLocale = isset($session->lang) ? $session->lang : $locale;
+        //  var_dump($langLocale);
+        // Set up and load the translations (all of them!)
+        $translate = new Zend_Translate(
+                        array(
+                            'adapter' => 'array',
+                            'content' => APPLICATION_PATH . '/languages/' . $langLocale . '.php',
+                            'locale' => $langLocale)
+        );
+
+        //$translate->setLocale($langLocale); // Use this if you only want to load the translation matching current locale, experiment.
+        // Save it for later
+        $registry = Zend_Registry::getInstance();
+        $registry->set('Zend_Translate', $translate);
+    }
 
 }
 
---
+++ application/languages/pl.php
@@ -0,0 +1,15 @@
+
+<?php
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+return array(
+    'Houseshare' => 'Stancje',
+    'Powered by the Zend Framework'=>'Zasilany przez Zend Framework',
+    'An error occurred' => 'Blad w aplikacji',
+    'Our header' => 'Nasz naglowek',
+    'Welcom in houseshare project'=>'Witam w projekcje stancje (house share)'
+    );
+?>
+
