bff6dc60826f120e8b808b6651eba597980970b5
marcin <marcin@marcin-mint>
2011-03-05 14:15:19 +0800

    Added My_Model_View_UserForPhoto.

---
+++ application/models/View/UserForAuth.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+class My_Model_View_UserForPhoto extends Zend_Db_Table_Abstract {
+
+    protected $_name = "VIEW_USER_FOR_PHOTO";
+    protected $_primary = array('user_id');
+}
+
+?>
