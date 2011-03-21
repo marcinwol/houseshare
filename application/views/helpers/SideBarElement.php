2d09b7cc72051315da0163bed0988d6b619e2efa
marcin <marcin@marcin-mint>
2011-03-21 20:37:49 +0800

    My_View_Helper_SideBarElement added as part of issue#8.

---
+++ application/views/helpers/SideBarElement.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of SideBarElement
+ *
+ * @author marcin
+ */
+class My_View_Helper_SideBarElement extends Zend_View_Helper_Abstract {
+    
+    const PLACEHOLDER_KEY = 'side-bar';
+
+    /**
+     * View instance
+     *
+     * @var  Zend_View_Interface
+     */
+    public $view;
+
+    public function sideBarElement($title = '', $content = '', array $options = array()) {
+
+        // get options if given
+        $place = isset($options['place']) ? $options['place'] : 'prepend';
+        $class = isset($options['class']) ? $options['class'] : '';
+        
+        // call partial to render side bar box/element
+        $html = $this->view->partial(
+                        '_partials/side-bar-box.phtml', null, 
+                         array('title' => $title, 'content' => $content, 'class' => $class)
+        );
+        
+        // put it into placeholder
+        $placeHolder = $this->getPlaceHolder();
+        if ('prepend' == $place) {
+            
+            $placeHolder->prepend($html);
+            
+        } else if ('append' == $place) {
+            
+            $placeHolder->captureStart();
+            echo $html;
+            $placeHolder->captureEnd();
+            
+        }
+        
+    }
+
+    /**
+     * Get the sidebar placeholder 
+     * 
+     * @return type Zend_View_Helper_Placeholder_Container
+     */
+    public function getPlaceHolder() {
+        return $this->view->placeholder(self::PLACEHOLDER_KEY);
+    }
+
+    /**
+     * Get Zend_View instance
+     *
+     * @param Zend_View_Interface $view
+     */
+    public function setView(Zend_View_Interface $view) {
+        $this->view = $view;
+    }
+
+}
+
+?>
