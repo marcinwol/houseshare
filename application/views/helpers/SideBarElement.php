0e06f2d4f97ce8b8fab14b69f458df51b625e5b5
marcin <marcin@marcin-mint>
2011-05-31 19:34:46 +0800

    My_View_Helper_SideBarElement refactored to allow translated content of the box be located in a partial view scripts in _sidebars folder.

--- application/views/helpers/SideBarElement.php
+++ application/views/helpers/SideBarElement.php
@@ -11,9 +11,10 @@
  * @author marcin
  */
 class My_View_Helper_SideBarElement extends Zend_View_Helper_Abstract {
-    
     const PLACEHOLDER_KEY = 'side-bar';
+    const SIDEBAR_DIR = '_sidebars';
 
+    protected $_lang = '';
     /**
      * View instance
      *
@@ -27,29 +28,43 @@ class My_View_Helper_SideBarElement extends Zend_View_Helper_Abstract {
         $place = isset($options['place']) ? $options['place'] : 'prepend';
         $class = isset($options['class']) ? $options['class'] : '';
         $addBreak = isset($options['break']) ? true : false;
-        
+
+
+        if (false !== ($bname = $this->_ifFromFile($content))) {
+            $this->_setLanguage();
+            $localeBname = $bname . '-' . $this->_lang;
+            $filePath = self::SIDEBAR_DIR . "/$localeBname.phtml";
+            
+            $scriptPath = APPLICATION_PATH . "/views/scripts/" . $filePath;            
+            
+            if (!file_exists($scriptPath)) {
+                // if locale sidebar partial don't exist, than use the default 
+                // one (i.e. english one).
+                $filePath = self::SIDEBAR_DIR . "/$bname.phtml";
+            }            
+            
+            $content = $this->view->partial($filePath);
+        }
+
         // call partial to render side bar box/element
         $html = $this->view->partial(
-                        '_partials/side-bar-box.phtml', null, 
-                         array('title' => $title, 'content' => $content, 'class' => $class)
+                        '_partials/side-bar-box.phtml', null, array('title' => $title, 'content' => $content, 'class' => $class)
         );
-        
+
         // put it into placeholder
         $placeHolder = $this->getPlaceHolder();
-        
+
         if (true === $addBreak) {
             $html .= '<div>&nbsp</div>';
         }
-        
-        if ('prepend' == $place) {            
+
+        if ('prepend' == $place) {
             $placeHolder->prepend($html);
-            
-        } else if ('append' == $place) {            
+        } else if ('append' == $place) {
             $placeHolder->captureStart();
             echo $html;
-            $placeHolder->captureEnd();            
+            $placeHolder->captureEnd();
         }
-        
     }
 
     /**
@@ -68,6 +83,43 @@ class My_View_Helper_SideBarElement extends Zend_View_Helper_Abstract {
      */
     public function setView(Zend_View_Interface $view) {
         $this->view = $view;
+    }
+
+    /**
+     * Check if content should be read from file.
+     * It is read from file if $str is in the format:
+     * "partialfile:name-of-partial" 
+     * 
+     * Note: name-of-partial is without .phtml extension.
+     * 
+     * 
+     * 
+     * @param string|false $str base name of sidebar partial or false
+     */
+    protected function _ifFromFile($str) {
+
+        $parts = array();
+
+        if (strpos($str, 'partialfile') === 0) {
+            $parts = explode(':', $str);
+        }
+
+        if (empty($parts)) {
+            return false;
+        }
+
+        return $parts[1];
+    }
+
+    protected function _setLanguage() {
+        /* @var $locale Zend_Locale */
+        $locale = Zend_Registry::get('Zend_Locale');
+        $lang = $locale->getLanguage();
+        
+        if ($lang != 'en') {
+            // if 'en' do not need to change the default settings.
+            $this->_lang = $lang;
+        }
     }
 
 }
